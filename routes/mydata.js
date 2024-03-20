f38ba4ae3ecd9392b6fd62ed2a790bf13ac134b9
maneesha karamsetty <karamsettymaneesha@gmail.com>
2024-03-20 15:54:53 -0500

    first commit

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Maneesha Karamsetty' });
+  res.render('mydata', { title: 'Maneesha Karamsetty' });
 });
 
 module.exports = router;
