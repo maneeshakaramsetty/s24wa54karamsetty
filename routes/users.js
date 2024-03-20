a84a2e63e9e592e75de2043806037e96b83ece06
maneesha karamsetty <karamsettymaneesha@gmail.com>
2024-03-20 15:57:32 -0500

    first commit

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var factor = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  factor-factor+1
+  factor=factor+1
   let result = product*factor;
   res.send(`Product is : ${result}`);
 });
